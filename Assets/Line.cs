e6c85e3e4237d72a7e73c549469f8b73bc8a8656
TakaiKentarou <mmpm664@yahoo.co.jp>
2021-09-14 17:33:01 +0900

    色々やった

---
+++ Assets/Line.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Line : MonoBehaviour
+{
+    private Rigidbody2D physics = null;
+
+    [SerializeField]
+    private LineRenderer direction = null;// 発射方向
+
+    private const float MaxMagnitude = 3f;// 最大付与力量
+
+    private Vector2 currentForce = Vector2.zero;// 発射方向の力
+
+    private Camera mainCamera = null;// メインカメラ
+
+    private Transform mainCameraTransform = null;// メインカメラ座標
+
+    private Vector2 dragStart = Vector2.zero;
+
+
+    void Start()
+    {
+        this.physics = this.GetComponent<Rigidbody2D>();
+        this.mainCamera = Camera.main;
+        this.mainCameraTransform = this.mainCamera.transform;
+    }
+
+
+    /// <summary>
+    /// マウス座標をワールド座標に変換して取得
+    /// </summary>
+    /// <returns></returns>
+    private Vector2 GetMousePosition()
+    {
+        // マウスから取得できないZ座標を補完する
+        var position = Input.mousePosition;
+        //position.z = this.mainCameraTransform.position.z;
+        position = this.mainCamera.ScreenToWorldPoint(position);
+        //position.z = 0;
+
+        return position;
+    }
+
+    /// <summary>
+    /// ドラック開始イベントハンドラ
+    /// </summary>
+    public void OnMouseDown()
+    {
+        this.dragStart = this.GetMousePosition();
+
+        this.direction.enabled = true;
+        this.direction.SetPosition(0, this.physics.position);
+        this.direction.SetPosition(1, new Vector2(this.physics.position.x, this.physics.position.y * -1));
+
+    }
+
+    /// <summary>
+    /// ドラッグ中イベントハンドラ
+    /// </summary>
+    public void OnMouseDrag()
+    {
+        var position = this.GetMousePosition();
+        this.currentForce = position - this.dragStart;
+        if (this.currentForce.magnitude > MaxMagnitude * MaxMagnitude)
+        {
+            this.currentForce *= MaxMagnitude / this.currentForce.magnitude;
+        }
+
+        this.direction.SetPosition(0, this.physics.position);
+        this.direction.SetPosition(1, new Vector2((this.physics.position.x + this.currentForce.x), this.physics.position.y + this.currentForce.y) * -1);
+    }
+
+    /// <summary>
+    /// ドラッグ終了イベントハンドラ
+    /// </summary>
+    public void OnMouseUp()
+    {
+        this.direction.enabled = false;
+        this.Flip(this.currentForce * -6f); //引っ張った方の逆にぶっ飛ばす
+    }
+
+    /// <summary>
+    /// ボールをはじく
+    /// </summary>
+    /// <param name="force"></param>
+    public void Flip(Vector2 force)
+    {
+        // 瞬間的に力を加えてはじく
+        this.physics.AddForce(force, ForceMode2D.Impulse);
+    }
+}
--- Assets/player.cs
+++ Assets/player.cs
@@ -4,38 +4,58 @@ using UnityEngine;
 
 public class player : MonoBehaviour
 {
-    private Rigidbody rb;
-    private const float MaxMagnitude = 2f;
-    private Vector2 force;
-    private Camera mainCamera;
-    private Vector3 dragPos = Vector2.zero;
+    Rigidbody2D rb2d;
+    Vector2 s_Vec;
+    Vector2 m_Vec;
+    private float speed;
 
-    public void Start()
+    private Camera mainCamera = null;
+    [SerializeField]
+    private LineRenderer direction = null;
+
+    void Start()
     {
-        rb = this.GetComponent<Rigidbody>();
-        mainCamera = Camera.main;
+        this.rb2d = GetComponent<Rigidbody2D>();
+        this.speed = 500;
+        this.mainCamera = Camera.main;
     }
-
-    public void OnMouseDown()
+    private Vector2 GetMousePosition()
     {
+        // マウスから取得できないZ座標を補完する
         var position = Input.mousePosition;
-        position.z = mainCamera.transform.position.z;
-        position = mainCamera.ScreenToWorldPoint(position);
-        position.y = 0;
-        dragPos = position;
+        //position.z = this.mainCameraTransform.position.z;
+        position = this.mainCamera.ScreenToWorldPoint(position);
+        //position.z = 0;
+
+        return position;
+    }
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            this.s_Vec = Input.mousePosition;
+            this.m_Vec = this.GetMousePosition();
+
+            this.direction.enabled = true;
+            this.direction.SetPosition(0, this.rb2d.position);
+            //this.direction.SetPosition(1, new Vector2(this.rb2d.position.x, this.rb2d.position.y * -1));
+            //this.direction.SetPosition(0, this.rb2d.position);
+            this.direction.SetPosition(1, new Vector2((this.rb2d.position.x + this.m_Vec.x), this.rb2d.position.y + this.m_Vec.y) * -1);
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            Vector2 endPos = Input.mousePosition;
+            Vector2 startDirection = -1 * (endPos - s_Vec).normalized;
+            this.rb2d.AddForce(startDirection * speed);
+            this.direction.enabled = false;
+        }
+        
     }
 
-    public void OnMouseDrag()
+    void FixedUpdate()
     {
-        var position = Input.mousePosition;
-        position.z = mainCamera.transform.position.z;
-        position = mainCamera.ScreenToWorldPoint(position);
-        position.y = 0;
-        force = position - dragPos;
-    }
-
-    public void OnMouseUp()
-    {
-        rb.AddForce(force * 5f, ForceMode.Impulse);
+        this.rb2d.velocity *= 0.995f;
     }
 }
+
+
